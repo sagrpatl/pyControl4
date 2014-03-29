2eba3648a89772b897b397e535491f6d3f13cf0b
Sagar Patel <saagarpatel@gmail.com>
2014-03-29 12:41:13 -0700

    initial commit

---
+++ Modules/Control4Lights.py
@@ -0,0 +1,10 @@
+'''
+Author: sapatel91
+Date: March 29, 2014
+File: Control4Lights.py
+
+Purpose: Encapsulate Control4 Lights
+
+Disclaimer: USE AT YOUR RISK, I TAKE NO RESPONSIBILITY
+            Most likely there won't be any though
+'''
