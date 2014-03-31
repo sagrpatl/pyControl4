cca93f2be775563592a20ad837003963807be3e3
Sagar Patel <saagarpatel@gmail.com>
2014-03-30 18:13:32 -0700

    initial commit

---
+++ TestSendSOAP.py
@@ -0,0 +1,22 @@
+'''
+Author: sapatel91
+Date: March 30, 2014
+File: TestSendSOAP.py
+
+Purpose: Send SOAP Commands to C4
+
+Disclaimer: USE AT YOUR RISK, I TAKE NO RESPONSIBILITY
+            Most likely there won't be any though
+'''
+
+from Modules.PyControl4 import *
+import socket
+import time
+
+# Establish Connection
+# NOTE: IP Address will be different for your system
+C4SoapConn('192.168.1.10', 5020)
+
+# Pulse Volume Down in Family Room
+Message = '<c4soap name="SendToDeviceAsync" async="1" seq="1615"><param name="iddevice" type="number">10</param><param name="data" type="string"><devicecommand><command>PULSE_VOL_DOWN</command><params></params></devicecommand></param></c4soap>'
+C4SoapConn.Send(Message)
